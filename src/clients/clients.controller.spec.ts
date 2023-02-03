9c4175b36dec3f2eff8c6492e874b77993c0bfae
Danilo Alves <33788984+danilofas@users.noreply.github.com>
2023-02-03 00:40:15 -0300

    feat(inclusao_test, #2): add tests

--- src/clients/clients.controller.spec.ts
+++ src/clients/clients.controller.spec.ts
@@ -1,20 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
+import { ClientsController } from './controllers/clients.controller';
 
 describe('ClientsController', () => {
   let controller: ClientsController;
+
+  const mockClientsService = {
+    create: jest.fn((dto) => {
+      return {
+        ...dto,
+      };
+    }),
+    update: jest.fn().mockImplementation((id, dto) => {
+      return {
+        id,
+        ...dto,
+      };
+    }),
+    delete: jest.fn().mockImplementation((id) => {
+      return {
+        id,
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ClientsController],
       providers: [ClientsService],
-    }).compile();
+    })
+      .overrideProvider(ClientsService)
+      .useValue(mockClientsService)
+      .compile();
 
     controller = module.get<ClientsController>(ClientsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be create a client', () => {
+    const dto = {
+      name: 'João',
+      cpf: '12345678901',
+      email: 'joaoteste@gmail.com',
+      address: 'Rua teste',
+      city: 'São Paulo',
+      phone: '11999999999',
+    };
+
+    expect(controller.create(dto)).toEqual({
+      name: dto.name,
+      cpf: dto.cpf,
+      email: dto.email,
+      address: dto.address,
+      city: dto.city,
+      phone: dto.phone,
+    });
+
+    expect(mockClientsService.create).toBeCalledWith(dto);
+  });
+
+  it('should update client', () => {
+    const dto = {
+      name: 'João',
+      cpf: '12345678901',
+      email: 'joaoteste@gmail.com',
+      address: 'Rua teste',
+      city: 'São Paulo',
+      phone: '11999999999',
+    };
+
+    expect(
+      controller.update('56a8c2b7-94de-4110-85b0-2ba21685ea54', dto),
+    ).toEqual({
+      id: '56a8c2b7-94de-4110-85b0-2ba21685ea54',
+      ...dto,
+    });
+
+    expect(mockClientsService.update).toHaveBeenCalled();
   });
 });
--- src/clients/clients.service.spec.ts
+++ src/clients/clients.service.spec.ts
@@ -1,12 +1,30 @@
+import { getModelToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientsService } from './clients.service';
+import { Client } from './entities/client.entity';
 
 describe('ClientsService', () => {
   let service: ClientsService;
 
+  const mockClientsModel = {
+    create: jest.fn().mockImplementation((dto) => Promise.resolve(dto)),
+    update: jest.fn().mockImplementation((id, dto) => {
+      return {
+        id,
+        ...dto,
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientsService],
+      providers: [
+        ClientsService,
+        {
+          provide: getModelToken(Client),
+          useValue: mockClientsModel,
+        },
+      ],
     }).compile();
 
     service = module.get<ClientsService>(ClientsService);
@@ -14,5 +32,47 @@ describe('ClientsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be create a client', async () => {
+    const dto = {
+      name: 'João',
+      cpf: '00891310100',
+      email: 'joaoteste@gmail.com',
+      address: 'Rua teste',
+      city: 'São Paulo',
+      phone: '11999999999',
+    };
+
+    expect(await service.create(dto)).toEqual(dto);
+  });
+
+  it('should be update a client', async () => {
+    const dto = {
+      id: '56a8c2b7-94de-4110-85b0-2ba21685ea54',
+      name: 'João',
+      cpf: '00891310100',
+      email: 'joaoteste@gmail.com',
+      address: 'Rua teste',
+      city: 'São Paulo',
+      phone: '11999999999',
+    };
+
+    expect(
+      await service.update('56a8c2b7-94de-4110-85b0-2ba21685ea54', dto),
+    ).toEqual({
+      id: {
+        address: 'Rua teste',
+        city: 'São Paulo',
+        cpf: '00891310100',
+        email: 'joaoteste@gmail.com',
+        id: '56a8c2b7-94de-4110-85b0-2ba21685ea54',
+        name: 'João',
+        phone: '11999999999',
+      },
+      where: {
+        id: '56a8c2b7-94de-4110-85b0-2ba21685ea54',
+      },
+    });
   });
 });
